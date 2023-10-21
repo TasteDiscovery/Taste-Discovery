ffc1d3abd5bdf419689b8c06126129c4b84c5a33
Johnny Miguel Sebastian Roque Neciosup <49285362+symphony701@users.noreply.github.com>
2023-10-21 01:35:49 -0500

    restaurante implementado con colisiones

--- Scenes/player/Player.gd
+++ Scenes/player/Player.gd
@@ -1,12 +1,7 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D
-@export var speed: int = 100
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const speed = 100
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = moveDirection * speed
@@ -15,11 +10,11 @@ func updateAnimation():
 	if velocity.length() == 0:
 		anim.stop()
 	else:
-		var direction = "forward"
+		var direction = "back"
 		if velocity.x < 0 :direction = "left"
 		elif velocity.x > 0 : direction = "right"
-		elif velocity.y < 0 : direction = "backward"
-		anim.play("walk_" + direction)
+		elif velocity.y < 0 : direction = "front"
+		anim.play(direction+"_walk")
 
 
 func _physics_process(delta):
