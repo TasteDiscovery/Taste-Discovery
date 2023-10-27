ff7f6c16096cba5eaba495296ee99b4ea0d5d789
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-10-26 20:38:11 -0500

    update design

--- Scenes/Farm/Map/farm_map.gd
+++ Scenes/Farm/Map/farm_map.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 @onready var cow_scene = preload("res://Scenes/Farm/NPC/Cow.tscn")
 #Vector(x,y)
-var positions = [Vector2(300,-550),
-	Vector2(180,-450),Vector2(250,-350),
-	Vector2(400,-700),Vector2(500,-700),
-	Vector2(600,-700)]
+var positions = [Vector2(180,-400),
+	Vector2(360,-500),Vector2(540,-550),
+	Vector2(720,-525),Vector2(630,-350),
+	Vector2(675,-150),Vector2(360,-300),
+	Vector2(360,-100),Vector2(900,-200)]
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in 9:
 		var cow_instance = cow_scene.instantiate()
 		add_child(cow_instance)
-		cow_instance.position = Vector2(randi() % 550 + 180,-(randi() % 550 + 10))
+		cow_instance.position = positions[x]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
